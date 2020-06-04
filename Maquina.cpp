9ac705d87158227fc495c22ee9762dce47db5722
Breno Augusto <brenomurige@outlook.com>
2020-06-03 22:53:08 -0300

    Add files via upload
    Maquina nova da Polishop que vende comida v0.2
    
    Mudanças:
    
    -Adicionei a possibilidade de logar como ADM, com senha e redefinição da mesma se preciso for.
    
    Por enquanto não adicionei mais nada na utilidade de ADM.

--- Maquina.cpp
+++ Maquina.cpp
@@ -86,9 +86,121 @@ int main (){
 	}
 	
 	
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------	
+
+//Variaveis da parte de ADM
+	
+int j = 3;				//j = numero de tentativas de senha
+double padrao = 123;	//padrao = senha padrao do sistema
+double senha;			//senha = entrada do usuario para fazer a comparação com o padrao
+
+//Inicio ADM
+
+if (cod == 'A' || cod == 'a'){
+		
+	cout << "Digite a senha cadastrada, para redefini-la, aperte (1) "<< endl;
+	cin >> senha;
 	
 	
+	//Arvore codicional para redefinição de senha
+	if (senha == 1){
+			
+		cout << "Digite a senha antiga" << endl;
+		cin >> senha;
+		
+		if (senha != padrao){
+			
+			while(senha != padrao){
+			
+			
+				cout << "Senha errada! Voce tem mais " << j << " tentativas "<< endl;
+				cin >> senha;
+				j--;
+			
+			
+				if (j < 0){
+					
+					cout << "Numero de tentativas excedidas! Cai fora.";
+					exit(0);
+					
+				}
+			}
+			//Laço para determinar o numero de tentativas para se colocar a senha certa
+			
+		}
+		
+		if (senha == padrao){
+			
+			cout << "Digite a senha nova! (Apenas numeros)" << endl;
+			cin >> padrao;
+			
+			
+			if (padrao == 1){
+				
+				do{
+					
+					cout << "Voce pode escolher qualquer senha, desde que seja apenas numeros e NÃO SEJA 1" << endl;
+					cin >> padrao;
+					
+				}while (padrao == 1);
+				
+			}
+			//Erro pra quando a senha for == 1
+			
+			
+			cout << "Digite a senha cadastrada" << endl;
+			cin >> senha;
+			
+			
+			while(senha != padrao){
+			
+			cout << "Senha errada! Voce tem mais " << j << " tentativas "<< endl;
+			cin >> senha;
+			j--;
+				
+				
+				if (j < 0){
+					
+					cout << "Numero de tentativas excedidas! Cai fora.";
+					exit(0);
+					
+				}
+				
+			}
+			//Segundo laço para caso a senha nova for errada
+		}
+		//Condicional para quando a senha antiga for correta
+	}
+	//Arvore codicional para redefinição de senha
+	
+	if (senha != padrao){
+		
+		do{
+
+		cout << "Senha errada! Se não lembrar, digite o RA do criador" << endl;
+		cin >> senha;
+		
+			if (senha == 2840482011030){
+				
+				break;
+			
+			}
+		
+		
+		}while (senha != padrao);
+	}
+		
 	
 	
+
+
+
+
+
+
+
+	cout << "Bem-vindo!";
+}
+
 	return 0;
 }
